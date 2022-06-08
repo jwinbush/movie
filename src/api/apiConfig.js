21734aded92d3e1433cd5e1fd353cac760631c5e
J.M Winbush <102334772+jwinbush@users.noreply.github.com>
2022-06-08 12:25:57 -0400

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '442496e73bc5a465bccb5697f24a15fb',
+    apiKey: 'find your api on TMBD',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
